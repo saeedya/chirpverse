418e66ce334598567eef878428a7e6f95edf758a
Saeed <yasrebi.saeed@gmail.com>
2024-06-27 21:13:06 +0100

    adding sqlalchemy structure.

--- ccs/ccs/ccs.py
+++ ccs/ccs/ccs.py
@@ -1,7 +1,12 @@
 from flask import Flask, Blueprint
 from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 from ccs.config import Config
+
+db = SQLAlchemy()
+mg = Migrate()
 
 apiv1_bp = Blueprint("apiv1_bp", __name__, url_prefix="/api/v1")
 apiv1 = Api(apiv1_bp)
@@ -12,4 +17,6 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     app.register_blueprint(apiv1_bp)
+    db.init_app(app)
+    mg.init_app(app, db)
     return app
--- ccs/ccs/config/config.py
+++ ccs/ccs/config/config.py
@@ -6,8 +6,13 @@ secret_key = secrets.token_urlsafe(32)
 
 
 class Config:
-
+    ####### Application Configs #######
     ENV = environ.get("CHIRPVERSE_CCS_ENV", "production")
     DEBUG = bool(int(environ.get("CHIRPVERSE_CCS_DEBUG", "0")))
     TESTING = bool(int(environ.get("CHIRPVERSE_CCS_TESTING", "0")))
     SECRET_KEY = secret_key
+    ####### Database Configs ########
+    SQLALCHEMY_DATABASE_URI = environ.get('CHIRPVERSE_CCS_DATABASE_URI', None)
+    SQLALCHEMY_TRACK_MODIFICATIONS = DEBUG
+    SQLALCHEMY_ECHO = DEBUG
+    SQLALCHEMY_RECORD_QUERIES = DEBUG
