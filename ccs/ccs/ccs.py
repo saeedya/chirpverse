bbe337133b324c0950737510f37d79c97a8d27de
Saeed <yasrebi.saeed@gmail.com>
2024-06-27 22:07:32 +0100

    adding marshmallow package

--- ccs/ccs/ccs.py
+++ ccs/ccs/ccs.py
@@ -1,5 +1,6 @@
 from flask import Flask, Blueprint
 from flask_restful import Api
+from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -7,6 +8,7 @@ from ccs.config import Config
 
 db = SQLAlchemy()
 mg = Migrate()
+ma = Marshmallow()
 
 apiv1_bp = Blueprint("apiv1_bp", __name__, url_prefix="/api/v1")
 apiv1 = Api(apiv1_bp)
@@ -19,4 +21,5 @@ def create_app():
     app.register_blueprint(apiv1_bp)
     db.init_app(app)
     mg.init_app(app, db)
+    ma.init_app(app)
     return app
